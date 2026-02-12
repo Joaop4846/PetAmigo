a59241a01eac59c120e4d10af8927c328e43daae
Joap4846 <joaop4846@gmail.com>
2026-02-12 10:41:55 -0300

    fix: ajuste URL API para produção

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000/api"
+    : "https://petamigo.onrender.com/api";
+
 const api = axios.create({
-  baseURL: `http://${window.location.hostname}:3000/api`,
+  baseURL,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
